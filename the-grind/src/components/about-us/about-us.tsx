c4616af236534958fbf8a6de906a81f35c90c4b7
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-06-06 16:22:21 -0700

    add an about-us component to provide a description of the clan's goals.

---
+++ the-grind/src/components/about-us/about-us.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+interface AboutUsProps {
+    header: string;
+};
+
+const AboutUs: FC<AboutUsProps> = ({ header}) => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default AboutUs;
