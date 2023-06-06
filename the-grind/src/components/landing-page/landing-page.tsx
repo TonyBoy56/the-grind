5e7d1cbf3c962680d4e4ed8d3d324911a0e97e7a
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-06-06 16:22:38 -0700

    add a landing-page component.

---
+++ the-grind/src/components/landing-page/landing-page.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+interface LandingPageProps {
+    header: string;
+};
+
+const LandingPage: FC<LandingPageProps> = ({ header}) => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default LandingPage;
